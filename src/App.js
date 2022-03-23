9de0991ca2b9050278d43b49ff395aed19448e39
JayLiu123 <jayliu19@yahoo.com.tw>
2022-03-23 22:44:58 +0800

    colors and basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import icon from "./icon.png"; 
 import * as React from "react";
 import Box from "@mui/material/Box"; 
 import TextField from "@mui/material/TextField"; 
 import Slider from "@mui/material/Slider"; 
-import {createTheme} from "@mui/material/styles"; 
-import { ThemeProvider } from "@emotion/react";
+import Stack from "@mui/material/Stack"; 
+import {ThemeProvider, useTheme, createTheme} from "@mui/material/styles"; 
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline"
 const theme = createTheme({
   palette: {
     primary: {
@@ -15,6 +19,9 @@ const theme = createTheme({
       main: "#93adf7",
       contrastText: "#2e364d",
     },
+    background: {
+      default: "#FEFDE4",
+    }
   },
 });
 const marks = [
@@ -46,20 +53,27 @@ function App() {
   return (
     <>
         <ThemeProvider theme= {theme}>
-        <Box sx={{mt: 5, mx: "auto", width: 500}}>
-          <Slider
-            aria-label="Restricted values"
-            defaultValue={0}
-            valueLabelFormat={valueLabelFormat}
-            getAriaValueText={valuetext}
-            step={null}
-            valueLabelDisplay="on"
-            marks={marks}   
-          />
+          <CssBaseline />
+          <Box sx = {{bgcolor: "#E4F2FE", width: "100%", height: 100, alignItems: "cemter"}}> 
+            <Stack direction= "row" spacing = {2}>
+              <item> <Box sx={{mt:1}}> <img src = {icon} width= {80} height= {80} alt={""} ></img> </Box></item>
+              <item> <TextField sx = {{width: 700, mt: 2.5, ml: 20}} id="outlined-basic" label="Enter" variant="outlined" color= "primary"/> </item>
+              <item> <Button variant="contained" sx = {{py: 2, mt: 2.5}} color= "secondary">Add</Button> </item>
+            </Stack>
           </Box>
-          <Box sx = {{mt: 10, mx: "auto", width: 200}}>
-            <TextField id="outlined-basic" label="Enter" variant="outlined" color= "secondary"/>
-          </Box>
+          <Grid container direction="row" alignItems="center" wrap = "nowrap">
+              <Grid item sx={{mt: 5, mx: "auto", width: 600}}>
+                <Slider
+                aria-label="Restricted values"
+                defaultValue={0}
+                valueLabelFormat={valueLabelFormat}
+                getAriaValueText={valuetext}
+                step={null}
+                valueLabelDisplay="on"
+                marks={marks}   
+                />              
+              </Grid>
+          </Grid>
         </ThemeProvider>
     </>
   );
